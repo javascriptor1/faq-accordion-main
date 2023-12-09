877ffb765c549916cdf504f3c1002d758fbd71fe
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-09 19:47:26 +0300

    solution done

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-'use strict'
+"use strict";
 
-const questions = document.querySelectorAll('summary')
+const questions = document.querySelectorAll("summary");
 
-questions.forEach((question)=>{
-    question.addEventListener('click',()=>{
-        
-    })
-})
+questions.forEach((question) => {
+  console.log(questions);
+  question.addEventListener("click", () => {
+
+    if (question.parentElement.hasAttribute("open")) {
+      question.children[1].src = "assets/images/icon-plus.svg";
+    } else {
+      question.children[1].src = "assets/images/icon-minus.svg";
+    }
+  });
+});
